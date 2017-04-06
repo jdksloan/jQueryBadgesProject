9ca122719e44b5666a827761f06da99f9286249b
jdksloan <jdksloan@gmail.com>
2017-04-06 16:41:04 +0200

    Code School Badges
    Get’s and shows the Code School Badges will incorporate all of this in
    one.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/jdksloan.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function(course) {
+        $( "#badges" ).append( '<div class="course">' + "<h3>" +  course.title + "</h3>" + "<img src='" + course.badge + "''/>" + "<a class='btn btn-primary' href='" + course.url + "' target='_blank'>See Course </a>"  + "</div>" );
+      });
+    }
+  });
 });
